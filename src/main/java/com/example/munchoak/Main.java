4906ea844b59a46703c6c8882a704001475db349
Shuvra <shuchita318@gmail.com>
2025-10-05 20:13:02 +0600

    Update Main.java

--- src/main/java/com/example/munchoak/Main.java
+++ src/main/java/com/example/munchoak/Main.java
@@ -6,13 +6,19 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
     @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/munchoak/Login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Munchoak Restaurant Management");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Welcome.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+
+            stage.setTitle("Munchoak");
+            stage.setScene(scene);
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
