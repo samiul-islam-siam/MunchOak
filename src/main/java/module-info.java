ba41abb7c386c5903245ca81a7850bcb9e15f913
samiul-islam-siam <samiul908986@gmail.com>
2025-11-07 12:14:33 +0600

    history page update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
 module com.example.munchoak {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.sql;
 
     opens com.example.munchoak to javafx.fxml;
     opens com.example.login to javafx.fxml;
